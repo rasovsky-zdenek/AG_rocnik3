74ec0d12573eea93805a151ef4109607fb543697
Rasovsky Zdenek <zdenek.rasovsky@gmail.com>
2019-10-12 00:45:52 +0200

    Output text
    Output of text "Ahoj svete!"

--- TestJava/src/testjava/TestJava.java
+++ TestJava/src/testjava/TestJava.java
@@ -15,7 +15,7 @@ public class TestJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Ahoj světe!");
     }
     
 }
